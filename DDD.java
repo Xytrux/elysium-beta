6543bbe250ad61c76bf2fba0e24b6eac73a22a97
Armando Hinojosa <79881161+Xytrux@users.noreply.github.com>
2023-06-04 15:34:55 -0700

    added a fucking oval

--- DDD.java
+++ DDD.java
@@ -4,8 +4,12 @@ import javax.swing.JFrame;
 public class DDD extends JFrame
 {
 	static JFrame F = new DDD();
+	Screen ScreenObject = new Screen();
+	
+	
 	public DDD()
 	{
+		add(ScreenObject);
 		setUndecorated(true);
 		setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		setVisible(true);
