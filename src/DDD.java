c3e513fac2954f46180e62dfc66bc1da3d6fe18b
Armando Hinojosa <79881161+Xytrux@users.noreply.github.com>
2023-06-04 17:47:43 -0700

    Fix the JFrame

--- src/DDD.java
+++ src/DDD.java
@@ -1,21 +1,21 @@
+import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-public class DDD extends JFrame
-{
-	static JFrame F = new DDD();
-	Screen ScreenObject = new Screen();
+public class DDD 
+{	
+	static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	static JTextField TF;
 	
-	
-	public DDD()
-	{
-		add(ScreenObject);
-		setUndecorated(true);
-		setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		setVisible(true);
-	}
 	public static void main(String[] args)
 	{
-		
+		JFrame F = new JFrame();
+		Screen ScreenObject = new Screen();
+		F.add(ScreenObject);
+		F.setUndecorated(true);
+	    F.setSize(ScreenSize);
+		F.setVisible(true);
+		F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
